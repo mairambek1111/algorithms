f1e5ab845a625f038ae9760575bd513ecd80404e
mairambek1111 <148425046+mairambek1111@users.noreply.github.com>
2024-05-15 16:18:47 +0600

    Time: 130 ms (58.11%), Space: 57900000 (61.67%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var islandPerimeter = function(grid) {
+    let perimetr = 0
+    let rows = grid.length
+    let columns = grid[0].length
+  for (let i = 0; i < rows; i++) {
+   for (let j = 0; j < columns; j++) {
+    if (grid[i] [j] == 1) {
+        perimetr += 4 
+        
+    if ( i > 0 && grid[i-1][j] == 1) {
+        perimetr--
+    }
+    if (i < rows- 1  && grid[i+1] [j] == 1 ) {
+        perimetr--
+    }
+    if ( j > 0 && grid[i] [j-1] == 1 ) {
+        perimetr--
+    }
+     if (j <  columns-1 && grid[i] [j+1] == 1) {
+        perimetr--
+    }
+    
+   }
+    }
+      
+    }
+    return perimetr
+};
+
